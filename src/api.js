d2780491a56f066b23c400bc0f7a98d0ea89a08b
tim-hub <tim_w@outlook.com>
2018-06-02 09:21:18 +1200

    add api

--- src/api.js
+++ src/api.js
@@ -4,3 +4,8 @@ import { head, prop, curry, tryCatch } from 'ramda'
 const BASE_URL = 'https://blockchain.info/q'
 
 const lifetime = { maxAge: 1000 * 3600 }
+
+const getDifficulty = memoize (
+  fetch(`${BASE_URL}/getblockcount`).then(decode),
+  lifetime
+)
