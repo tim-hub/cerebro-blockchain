0487fe6b86ed2a614d83142294037e137c115bb0
tim-hub <tim_w@outlook.com>
2018-06-02 09:35:26 +1200

    add styles and preview

--- src/preview.js
+++ src/preview.js
@@ -1,3 +1,22 @@
-
 import React, { Component, PropTypes } from 'react'
-import { getWeather } from './api'
+import { getDificulty } from './api'
+import styles from './styles.scss'
+
+
+export default class Preview extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      difficulty: null,
+      error: null,
+    }
+  }
+
+  componentDidMount() {
+    getDificulty()
+      .then(difficulty => this.setState({ difficulty }))
+      .catch(error => this.setState({ error }))
+  }
+
+
+}
