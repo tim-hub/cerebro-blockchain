e3d79d3ad41c1b518d1475bbe5de3c7071fff9df
tim-hub <tim_w@outlook.com>
2018-06-02 09:40:07 +1200

    add display

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
+import { getDifficulty } from './api'
+
 export const fn = ({ term, display }) => {
   // Put your plugin code here
-  display({
-    title: `You've entered ${term}`
-  })
+  let match = term.match(/^blockchain?\s(.+)$/)
+  match = match || term.match(/^(.+)\sblockchain?$/)
+  if (match) {
+    let difficulty = getDifficulty();
+
+    display({
+      title: `Current Difficulty ${difficulty}`
+    })
+  }
 }
